dbca6582f749fd5bc9198993b130465b003f961e
sean <seanyi433@gmail.com>
2023-04-08 19:49:23 -0400

    lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 62
+let num2 = 1
+let num3 = 24
+let num4 = 5
+const multiply = (num1 * num2)
+const random = Math.floor(Math.random()) + 1
+const mod = Math.floor(num3/num4)
+const max = Math.max(1.2,3,4,20)
